85112fa08140d06cb562324e7d792f1fd6452537
Juan Tareq <juantareq@MacBook-Pro-de-Juan.local>
2014-05-02 14:55:18 +0100

    pruebas, tabla de simbolos funciona

--- public/test/tests.js
+++ public/test/tests.js
@@ -1,36 +1,36 @@
 var assert = chai.assert;
 
-suite('PL/0 Analyzer using Jison', function() {
+suite('PL/0 Analizador using Jison', function() {
 
-  test('Asociaciatividad de la resta', function () {
+  test('Resta', function () {
     var result = pl0.parse('var a; a=3-2-1.');
     assert.equal(result.content.right.left.left.value, 3);
     assert.equal(result.content.right.left.right.value, 2);
     assert.equal(result.content.right.left.type, '-');
   });
 
-  test('Asociaciatividad de la división', function () {
+  test('División', function () {
     var result = pl0.parse('var a; a=3/2/1.');
     assert.equal(result.content.right.left.left.value, 3);
     assert.equal(result.content.right.left.right.value, 2);
     assert.equal(result.content.right.left.type, '/');
   });
 
-  test('Sentencia IF', function () {
+  test('IF', function () {
     assert.equal(pl0.parse('var a, b, c; if (a < b) then c = 3.').content.type, 'IF');
   });
 
-  test('Sentencia IF-ELSE', function () {
+  test('IF-ELSE', function () {
     assert.equal(pl0.parse('var a, b, c; if (a < b) then c = 3 else c = a.').content.type, 'IFELSE');
   });
 
-  test('Dangling else', function () {
+  test('if then (if then (else)))', function () {
     var result = pl0.parse('var a, b, c, e; if (a < b) then if (c != e) then a = a+1 else a = b.');
     assert.equal(result.content.type, 'IF');
     assert.equal(result.content.st.type, 'IFELSE');
   });
 
-  test('PROCEDURE con parámetros', function () {
+  test('PROCEDURE(parametro)', function () {
     var result = pl0.parse('procedure proc (a, b, c); c = a+b;.');
     assert.equal(result.procs[0].type, 'PROCEDURE');
     assert.equal(result.procs[0].name, 'proc');
@@ -47,5 +47,28 @@ suite('PL/0 Analyzer using Jison', function() {
     assert.equal(result.procs[0].content.arguments.length, 3);
     assert.equal(result.procs[0].content.arguments[2].content.type, '*');
   });
+
+});
+
+suite('PL/0 Analizador ambito Jison', function() {
+    test('Tabla de símbolos', function() {
+        var result = pl0.parse('const a = 5; var b; procedure c; call c; b = a+7.');
+        assert.deepEqual(result.sym_table, {
+            a: {
+                type: "CONST VAR",
+                value: "5",
+                declared_in: "global"
+            },
+            b: {
+                type: "VAR",
+                declared_in: "global"
+            },
+            c: {
+                type: "PROCEDURE",
+                arglist_size: 0,
+                declared_in: "global"
+            }
+        });
+    });
 
 });
